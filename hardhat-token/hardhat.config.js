fb6db3a7de8c3d1520e9ebfbab5fbd725825a21c
Felix_Xie <xty15311790673@163.com>
2025-04-27 05:41:24 +0800

    settings and deploy messages

---
+++ hardhat-token/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const ALCHEMY_API_KEY  = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// This is a sample Hardhat task. You can create more tasks by creating new
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [ ROPSTEN_PRIVATE_KEY ]
+    }
+  }
+};
