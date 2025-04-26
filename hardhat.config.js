98b0ad5d5e51e48bb7eb21ea12847684bdffd092
Felix_Xie <xty15311790673@163.com>
2025-04-27 06:24:05 +0800

    update config settings(using CI only test on hardhat testchain)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const ALCHEMY_API_KEY  = process.env.ALCHEMY_API_KEY;
-const ROPSTEN_PRIVATE_KEY = process.env.PRIVATE_KEY;
+// const ALCHEMY_API_KEY  = process.env.ALCHEMY_API_KEY;
+// const ROPSTEN_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // This is a sample Hardhat task. You can create more tasks by creating new
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  networks: {
+/*  networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [ ROPSTEN_PRIVATE_KEY ]
     }
   }
+*/
 };
